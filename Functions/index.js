ff7c60c9c85d6cc7ccc3ab3e994933caf458b308
Neelima <neelimaakkiraju2001@gmail.com>
2024-07-18 12:16:13 +0530

    Learned Function calling methods

---
+++ Functions/index.js
@@ -0,0 +1,20 @@
+// how can you call a function in different ways?????????
+
+function greetings(functionCall){
+   const result = "Method" + functionCall
+   console.log(result)}
+//common method
+greetings("common")
+
+//call method
+greetings.call(this,"call")
+
+//aply method
+greetings.apply(this, ['apply'])
+
+//bind method
+var wish = greetings.bind(this,"bind")
+wish()
+
+//string literals
+greetings`String Literal`
