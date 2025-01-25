0aed6178419dead4cd61187d2dd45fadddfd103e
Neelima Akkiraju <neelimaakkiraju2001@gmail.com>
2025-01-25 16:07:58 +0530

    completed DOM manipulations

---
+++ manipulate text/DOMmanipulations.js
@@ -0,0 +1,11 @@
+const paragraphText = document.getElementById("text")
+paragraphText.textContent="Manipulated Paragraph"
+
+const button = document.getElementById("jsstyle")
+button.textContent="click me"
+
+function js_style(){
+    text.style.fontSize = "14pt";
+    text.style.fontFamily = "Comic Sans MS";
+    text.style.color = "green";
+}
