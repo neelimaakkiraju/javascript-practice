26bc40b676362a72c9d500389fbd5516d9e9741e
Neelima <105192660+neelimaakkiraju@users.noreply.github.com>
2025-03-21 17:13:40 +0530

    new inter view queston

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
-// how can you call a function in different ways?????????
+// // how can you call a function in different ways?????????
 
-function greetings(functionCall){
-   const result = "Method called : " + functionCall
-   console.log(result)}
-//common method
-greetings("common")
+// function greetings(functionCall){
+//    const result = "Method called : " + functionCall
+//    console.log(result)}
+// //common method
+// greetings("common")
 
-//call method
-greetings.call(this,"call")
+// //call method
+// greetings.call(this,"call")
 
-//aply method
-greetings.apply(this, ['apply'])
+// //aply method
+// greetings.apply(this, ['apply'])
 
-//bind method
-var wish = greetings.bind(this,"bind")
-wish()
+// //bind method
+// var wish = greetings.bind(this,"bind")
+// wish()
 
-//string literals
-greetings`String Literal`
+// //string literals
+// greetings`String Literal`
+
+
+let arr = [1,2,3,4][1,2,3]
+
+console.log(arr)
