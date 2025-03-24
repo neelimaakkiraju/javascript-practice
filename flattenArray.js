b063009f7d5eef5ed41791f1b340362e2cb7ab6e
Neelima <105192660+neelimaakkiraju@users.noreply.github.com>
2025-03-24 17:37:01 +0530

    shuffled array

---
+++ flattenArray.js
@@ -0,0 +1,5 @@
+let Arr = [1, [2], [3, [[4]]],[5,6]]
+
+let flatArr = Arr.flat()
+
+console.log(flatArr)
---
+++ shuffleArray.js
@@ -0,0 +1,8 @@
+let myArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+for(let i = 0 ; i<myArray.length;i++){
+    let result = Math.floor((Math.random() * i))
+    return result
+}
+let result = Math.floor((Math.random() * i))
+console.log(result)
