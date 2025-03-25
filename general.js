e983ba5ce2a5ccaaec15eac6377670a79c900e7a
Neelima <105192660+neelimaakkiraju@users.noreply.github.com>
2025-03-25 12:18:14 +0530

    array methiods prtactice

--- general.js
+++ general.js
@@ -1,25 +1,39 @@
-console.log(NaN == NaN)
+// console.log(NaN == NaN)
 
-console.log(NaN === NaN)
-
-
-
-
-function getLongest(str){
-    const result = str.split(" ")
-    const output = result.sort((b,a)=>b-a)
-    const finalResult =  output.reverse()
-    return finalResult[0]
-}
-
-console.log(getLongest("wewe ereuee rueyhwurye eruyeuryueweeee"))
+// console.log(NaN === NaN)
 
 
 
 
+// function getLongest(str){
+//     const result = str.split(" ")
+//     const output = result.sort((b,a)=>b-a)
+//     const finalResult =  output.reverse()
+//     return finalResult[0]
+// }
 
-//temporal dead zone
+// console.log(getLongest("wewe ereuee rueyhwurye eruyeuryueweeee"))
 
-function tdz(){
+
+
+
+
+// //temporal dead zone
+
+// function tdz(){
  
-}
+// }
+
+
+// console.log(a)
+
+// console.log(b)
+
+// var b = a = 12
+
+
+var c = []
+var d  = []
+
+console.log(c == d)
+console.log(c === d)
