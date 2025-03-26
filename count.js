cd1fa5d6d6c21c9f3bf9e9a4274c97137463cd78
Neelima <105192660+neelimaakkiraju@users.noreply.github.com>
2025-03-26 15:50:17 +0530

    new js practice

---
+++ count.js
@@ -0,0 +1,30 @@
+// var arr = [1,2,3,4,5]
+
+// arr.splice(0,1)
+// arr.unshift(8)
+
+// console.log(arr)
+
+
+const obj = {
+    a : 1,
+    b:  2,
+    c: 3,
+    "a": 7,
+    b: 5
+
+}
+
+console.log(obj)
+
+
+console.log(!"hello") //false
+console.log(+false) //0
+
+
+
+console.log("" == false) //true
+console.log("" === false) //false
+
+console.log(NaN == NaN)  //false
+console.log(NaN === NaN)  //false
